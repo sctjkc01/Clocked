402dcea05465651872be6d86b1b374134be88fc8
The Stick <avaera8820@gmail.com>
2013-05-26 01:23:10 -0400

    Moving things around...

---
+++ DarosGame/DarosGame/DarosGame/Resources.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace DarosGame {
+    public class Resources {
+        public static SpriteFont testfont;
+
+        public static void InitResources(ContentManager cm) {
+            testfont = cm.Load<SpriteFont>("test/testfont");
+        }
+    }
+}
