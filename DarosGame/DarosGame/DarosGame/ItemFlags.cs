238dfdb0baba1ef82d36512fca6d69a0c100c71e
The Stick <avaera8820@gmail.com>
2013-05-28 20:29:05 -0400

    Added Item Flags enum
    If there's more flags that need to be added, to describe items, then
    those go in this file.

---
+++ DarosGame/DarosGame/DarosGame/ItemFlags.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DarosGame {
+    public enum ItemFlags {
+        OLD, NEW, CRUDE, GOLD
+    }
+}
