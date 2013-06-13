d84267e0b305e5c8e3b5efdaf09af98fb18b5ffc
The Stick <avaera8820@gmail.com>
2013-06-13 16:24:11 -0400

    Beginnings of Conversation implementation.

---
+++ DarosGame/DarosGame/DarosGame/Convo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using StickXNAEngine.Graphic;
+
+namespace DarosGame {
+    namespace Convo {
+        public class Conversation {
+            Blurb curr;
+
+            public void Draw(SpriteBatch sb) {
+
+            }
+
+
+        }
+
+        public abstract class Blurb {
+            /// <summary>
+            /// A resource path to the portrait of the speaker of this blurb.
+            /// </summary>
+            protected string img;
+            /// <summary>
+            /// The speaker's name - will not show if empty ("") or null, or if player does not have necessary tool.
+            /// </summary>
+            protected string name;
+            /// <summary>
+            /// The message being said.
+            /// </summary>
+            protected string message;
+            /// <summary>
+            /// Is the message on the top or the bottom?
+            /// </summary>
+            protected bool top = false;
+
+            public void Draw(SpriteBatch sb) {
+
+            }
+        }
+
+        public class LinearBlurb : Blurb {
+            Blurb next;
+        }
+
+        public class BranchingBlurb : Blurb { }
+    }
+}
