2f01076964db47f936b2b5ac41e7419c640c5111
The Stick <avaera8820@gmail.com>
2013-06-21 23:42:05 -0400

    Menu implementation
    Pitched MenuItem in favor of continuing to use Button.

--- StickXNAEngine/StickXNAEngine/Input/Menu.cs
+++ StickXNAEngine/StickXNAEngine/Input/Menu.cs
@@ -2,9 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using StickXNAEngine.Utility;
+using Microsoft.Xna.Framework.Input;
 
 namespace StickXNAEngine.Input {
-    public class Menu {
+    public class Menu : IUpdating {
+        private List<Button> btns = new List<Button>();
 
+        public Menu() {
+            PostProcessing.Add(this);
+        }
+
+        public void Add(Button btn) {
+            btns.Add(btn);
+        }
+
+        public void Update(GameTime gt) {
+            MouseState ms = Mouse.GetState();
+            if(btns.Count > 0) {
+                foreach(Button alpha in btns) {
+                    alpha.Update(ms, gt);
+                }
+            }
+        }
+
+        public void Hide() {
+            if(btns.Count > 0) {
+                foreach(Button alpha in btns) {
+                    alpha.Visible = false;
+                    alpha.Active = false;
+                }
+            }
+        }
+
+        public void Show() {
+            if(btns.Count > 0) {
+                foreach(Button alpha in btns) {
+                    alpha.Visible = true;
+                    alpha.Active = true;
+                }
+            }
+        }
     }
 }
