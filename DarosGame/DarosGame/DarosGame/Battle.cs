2ab952dc009feaf52d1bd4bf3f2d446d54d1438c
The Stick <avaera8820@gmail.com>
2013-06-30 12:39:52 -0500

    Trying to git the current stuff.

---
+++ DarosGame/DarosGame/DarosGame/Battle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StickXNAEngine.Audio;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DarosGame {
+    namespace Battle {
+        public class BattleInstance {
+            private Song theme;
+
+            private List<BattleMob> turnOrder;
+
+            public BattleInstance(List<BattleMob> turnOrder) {
+                this.turnOrder = turnOrder;
+                
+            }
+
+            public void Update(GameTime gt) {
+
+            }
+
+            public void Draw(SpriteBatch sb) {
+
+            }
+        }
+
+        public abstract class BattleMob {
+            private int x, y;
+        }
+
+        public class BattleProtag : BattleMob {
+            private ProtagStats stats;
+
+            public BattleProtag(ProtagStats stats) {
+                this.stats = stats;
+            }
+        }
+    }
+}
